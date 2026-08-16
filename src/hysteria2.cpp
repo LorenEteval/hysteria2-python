70d62e5c77bf247860e053bf9cc24ef90c93a019
Loren Eteval <loren.eteval@proton.me>
2026-08-16 16:55:44 +0800

    Add Linux and Windows ARM wheel builds
    Signed-off-by: Loren Eteval <loren.eteval@proton.me>

--- src/hysteria2.cpp
+++ src/hysteria2.cpp
@@ -1,9 +1,25 @@
 #include <string>
+#if defined(__MINGW32__) && defined(_M_ARM64)
+    // CPython 3.14t uses MSVC's __getReg(18) intrinsic to read the Windows
+    // ARM64 thread environment block, but LLVM-MinGW does not provide it.
+    // Windows reserves x18 for that pointer, so provide the equivalent here.
+    #include <cstdint>
+    static inline std::uintptr_t getArm64ThreadPointer()
+    {
+        std::uintptr_t value;
+        __asm__ __volatile__("mov %0, x18" : "=r"(value));
+        return value;
+    }
+    #define __getReg(registerNumber) getArm64ThreadPointer()
+#endif
 #if defined _WIN64
     #define _hypot hypot
     #include <cmath>
 #endif
 #include <pybind11/pybind11.h>
+#if defined(__MINGW32__) && defined(_M_ARM64)
+    #undef __getReg
+#endif
 
 #include "hysteria2.h"
 
@@ -23,7 +39,9 @@ namespace {
         }
     }
 
-    PYBIND11_MODULE(hysteria2, m, py::mod_gil_not_used()) {
+    // TODO: Audit the C++ and Go code for free-threading safety before using
+    // py::mod_gil_not_used() here.
+    PYBIND11_MODULE(hysteria2, m) {
         m.def("startFromJSON",
             &startFromJSON,
             "Start Hysteria2 client with JSON",
